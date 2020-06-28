0a777b849aa54bb08e637be97b35b669a0990bf6
Einar Rosvoll <pigzelner@gmail.com>
2020-06-28 12:49:34 +0200

    Began work on workbench

--- app/src/main/java/net/merayen/kitchentimer/fragments/WorkbenchTab.kt
+++ app/src/main/java/net/merayen/kitchentimer/fragments/WorkbenchTab.kt
@@ -7,7 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import kotlinx.android.synthetic.main.item_setup_tab_fragment.view.*
+import androidx.recyclerview.widget.RecyclerView
 
 import net.merayen.kitchentimer.R
 import net.merayen.kitchentimer.viewmodels.WorkbenchTabViewModel
@@ -18,6 +18,43 @@ class WorkbenchTab : Fragment() {
          * Set this to make this view show a running task
          */
         var showRunningTaskId: Int? = null
+    }
+
+    class TaskListRecyclerViewAdapter : RecyclerView.Adapter<ViewHolder>() {
+        private val mOnClickListener: View.OnClickListener
+        var onItemClick: (() -> Unit)? = null
+
+        init {
+            mOnClickListener = View.OnClickListener {
+                onItemClick?.invoke()
+            }
+        }
+
+        override fun onCreateViewHolder(
+            parent: ViewGroup,
+            viewType: Int
+        ): ViewHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.fragment_workbench_task_listitem, parent, false)
+
+            return ViewHolder(view)
+        }
+
+        override fun getItemCount(): Int {
+            TODO("Not yet implemented")
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            TODO("Not yet implemented")
+        }
+    }
+
+    class ViewHolder(private val mView: View) : RecyclerView.ViewHolder(mView) {
+        //private val mContentView: TextView = mView.content
+
+        override fun toString(): String {
+            return "hohohoho" //super.toString() + " '" + mContentView.text + "'"
+        }
     }
 
     var showRunningTaskId: Int? = null
