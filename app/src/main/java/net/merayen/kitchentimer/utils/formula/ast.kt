0eb150e49bcd4a072bdaf9285db8f8d71ab1a282
Einar Rosvoll <pigzelner@gmail.com>
2020-07-23 18:10:45 +0200

    math formula parser seems to be able to parse now

--- app/src/main/java/net/merayen/kitchentimer/utils/formula/ast.kt
+++ app/src/main/java/net/merayen/kitchentimer/utils/formula/ast.kt
@@ -1,65 +1,104 @@
 package net.merayen.kitchentimer.utils.formula
 
+import java.lang.reflect.InvocationTargetException
 import kotlin.reflect.KClass
 
-abstract class Token(private var startPos: Int, private val text: String) {
+
+abstract class Token(val startPos: Int, private val text: String) {
+    class ParseFailed internal constructor() : RuntimeException()
+
     var pos = startPos
         private set
 
     private val children = ArrayList<Token>()
 
-    val isValid: Boolean
-        get() = startPos != pos
-
-    protected abstract fun onHandle(): Boolean
-
     fun <T : Token> consume(cls: KClass<out T>): T? {
-        val instance = cls.constructors.first().call(pos, text)
+        val instance: T
+        try {
+            instance = cls.constructors.first().call(pos, text)
+        } catch (exception: InvocationTargetException) {
+            if (exception.targetException is ParseFailed)
+                return null
+            else
+                throw exception
+        }
 
-        if (instance.onHandle()) {
-            if (instance.pos <= pos)
-                throw RuntimeException("${instance::class.simpleName} did not consume anything even though it reports it handled characters")
+        if (instance.pos <= pos)
+            throw RuntimeException("${instance::class.simpleName} did not consume anything. Forgotten to call fail()?")
 
-            pos = instance.pos
-            children.add(instance)
-            return instance
+        pos = instance.pos
+        children.add(instance)
+        return instance
+    }
+
+    fun consume(searchText: String): String? {
+        if (this.text.substring(pos).startsWith(searchText)) {
+            pos += searchText.length
+            return searchText
         }
 
         return null
     }
 
-    fun consume(text: String): Boolean {
-        if (text.substring(pos).startsWith(text)) {
-            pos += text.length
-            return true
+    fun consume(char: Char): Char? {
+        if (text[pos] == char) {
+            pos++
+            return char
         }
 
-        return false
+        return null
+    }
+
+    fun consume(chars: CharArray): String? {
+        var read = 0
+        do {
+            var hit = false
+            for (c in chars) {
+                if (text[pos + read] == c) {
+                    read += 1
+                    hit = true
+                }
+            }
+        } while (hit)
+
+        if (read == 0)
+            return null
+
+        val previousPos = pos
+        pos += read
+
+        return text.substring(previousPos, pos)
+    }
+
+    protected fun fail(): Nothing {
+        println("Parsing stopped at: ${Thread.currentThread().stackTrace[2]}")
+        throw ParseFailed()
     }
 }
 
 abstract class LiteralValue(pos: Int, text: String) : Token(pos, text)
 
 class Whitespace(pos: Int, text: String) : Token(pos, text) {
-    override fun onHandle(): Boolean {
-        var read = 0
-        while (consume(" ") || consume("\n") || consume("\t"))
-            read++
+    init {
+        var read = false
+        while (consume(" ") ?: consume("\n") ?: consume("\t") != null)
+            read = true
 
-        return read > 0
+        if (!read)
+            fail()
     }
 }
 
 class Statement(pos: Int, text: String) : Token(pos, text) {
-    lateinit var destination: Variable
+    var destination: Variable
         private set
 
-    override fun onHandle(): Boolean {
+    init {
         consume(Whitespace::class)
-        destination = consume(Variable::class) ?: return false
-        consume(Assign::class) ?: return false
+        destination = consume(Variable::class) ?: fail()
+        consume(Assign::class) ?: fail()
         consume(Whitespace::class)
-        consume(Expression::class)
+        consume(Expression::class) ?: fail()
     }
 }
 
@@ -67,49 +106,57 @@ class Expression(pos: Int, text: String) : Token(pos, text) {
     lateinit var destination: Variable
         private set
 
-    override fun onHandle(): Boolean {
+    init {
         var read = false
-        while (consume(Variable::class) != null || consume(Number::class) != null) {
+        while (consume(Variable::class) ?: consume(Number::class) != null) {
             read = true
-            if (consume(Operator::class) == null)
-                break
+
+            consume(Whitespace::class)
+            consume(Operator::class) ?: break
+            consume(Whitespace::class)
         }
 
-        return read
+        if (!read)
+            fail()
     }
 }
 
 class Number(pos: Int, text: String) : LiteralValue(pos, text) {
-    override fun onHandle(): Boolean {
-        TODO("Not yet implemented")
+    init {
+        var read = false
+        while ("0123456789.".map { consume(it) }.any { it != null })
+            read = true
+
+        if (!read)
+            fail()
     }
 }
 
 class Operator(pos: Int, text: String) : Token(pos, text) {
-    enum class Type(val code: String) {
-        ADD("+"),
-        SUB("-"),
-        MUL("*"),
-        DIV("/")
-    }
+    enum class Type(val code: String) { ADD("+"), SUB("-"), MUL("*"), DIV("/") }
 
-    override fun onHandle(): Boolean {
-        TODO("Not yet implemented")
+    init {
+        Type.values().firstOrNull { consume(it.code) != null } ?: fail()
     }
 }
 
 class Assign(pos: Int, text: String) : Token(pos, text) {
-    override fun onHandle(): Boolean {
-        TODO("Not yet implemented")
+    init {
+        consume("=") ?: fail()
     }
 }
 
 class Variable(pos: Int, text: String) : Token(pos, text) {
-    override fun onHandle(): Boolean {
-        TODO("Not yet implemented")
+    companion object {
+        private val allowedCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_ .".toCharArray()
     }
-}
 
-fun parse(text: String): Statement {
-
+    init {
+        consume("[") ?: fail()
+        println("Kom hit A")
+        consume(allowedCharacters) ?: fail()
+        println("Kom hit B")
+        consume("]") ?: fail()
+        println("Kom hit C")
+    }
 }
--- app/src/main/java/net/merayen/kitchentimer/utils/formula/parser.kt
+++ app/src/main/java/net/merayen/kitchentimer/utils/formula/parser.kt
@@ -1,11 +1,19 @@
 package net.merayen.kitchentimer.utils.formula
 
+class CanNotParse : RuntimeException()
+
 /**
  * Parses math formula text strings.
  * This is custom as heck.
  */
-fun parse(text: String): Expression {
-    val result = Statement(0, text).onHandle()
-    result.handle(text)
-    return result
+fun parse(text: String): Token {
+    try {
+        return Statement(0, text)
+    } catch (e: Token.ParseFailed) { }
+
+    try {
+        return Expression(0, text)
+    } catch (e: Token.ParseFailed) { }
+
+    throw CanNotParse()
 }
