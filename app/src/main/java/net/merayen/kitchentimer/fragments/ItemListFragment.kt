b47eb7ad81b99cde137e8a7615e7ac93f6df0958
Einar Rosvoll <pigzelner@gmail.com>
2020-08-30 22:37:17 +0200

    Item list

--- app/src/main/java/net/merayen/kitchentimer/fragments/ItemListFragment.kt
+++ app/src/main/java/net/merayen/kitchentimer/fragments/ItemListFragment.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.fragment_item_list.view.*
 import net.merayen.kitchentimer.R
+import net.merayen.kitchentimer.data.Item
 import net.merayen.kitchentimer.viewmodels.ItemListViewModel
 
 private const val ARG_PARAM1 = "param1"
@@ -18,39 +21,68 @@ class ItemListFragment : Fragment() {
     private var param1: String? = null
     private val viewModel by viewModels<ItemListViewModel>()
 
+    private var items: List<Item> = ArrayList()
+
     class MyViewHolder(val view: View) : RecyclerView.ViewHolder(view)
 
-    class MyAdapter : RecyclerView.Adapter<MyViewHolder>() {
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = MyViewHolder(
-            // Instantiates the fragment_task_list layout xml for each item in the list
-            LayoutInflater
-                .from(parent.context)
-                .inflate(R.layout.fragment_item_list, parent, false)
-        )
+    inner class MyAdapter : RecyclerView.Adapter<MyViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+            viewModel.get().observe(viewLifecycleOwner, Observer {
+                items = it
+                println("Loaded data: ${items.size}")
+                notifyDataSetChanged()
+            })
+
+            return MyViewHolder(
+                // Instantiates the fragment_task_list layout xml for each item in the list
+                LayoutInflater
+                    .from(parent.context)
+                    .inflate(R.layout.fragment_item_list_list_item, parent, false)
+            )
+        }
 
         override fun getItemCount(): Int {
-            return 4
+            println("Got size: ${items.size}")
+            return items.size
         }
 
         override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
             println("MyAdapter position: $position")
             println(holder.view.itemList)
+
+            val data = items[position]
+
+            holder.view.findViewById<TextView>(R.id.itemName).text = data.name
+
+            with(holder) {
+                // TODO
+            }
         }
     }
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
         }
-
-        viewModel.get().observe(this, Observer {
-
-        })
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? = inflater.inflate(R.layout.fragment_item_list, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_item_list, container, false)
+        val itemList = view.findViewById<RecyclerView>(R.id.itemList)
+
+        with(itemList) {
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+            adapter = MyAdapter()
+        }
+
+        viewModel.get().observe(viewLifecycleOwner, Observer {
+            items = it
+            itemList.adapter!!.notifyDataSetChanged()
+        })
+
+        return view
+    }
 
     companion object {
         @JvmStatic
