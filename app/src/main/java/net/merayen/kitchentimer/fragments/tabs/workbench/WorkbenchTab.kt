d1ed9de490e45693a01cb641a22c1478c7c038e0
Einar Rosvoll <pigzelner@gmail.com>
2020-09-24 14:41:55 +0200

    bad rename refactoring by IntelliJ?

--- app/src/main/java/net/merayen/kitchentimer/fragments/tabs/workbench/WorkbenchTab.kt
+++ app/src/main/java/net/merayen/kitchentimer/fragments/tabs/workbench/WorkbenchTab.kt
@@ -8,9 +8,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import net.merayen.kitchentimer.R
+import net.merayen.kitchentimer.fragments.tabs.workbench.RunningTimersListFragment
 import net.merayen.kitchentimer.livedata.RunningTaskData
 import net.merayen.kitchentimer.viewmodels.WorkbenchTabViewModel
 
